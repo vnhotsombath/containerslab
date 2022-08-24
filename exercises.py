b6dc9c81ec6d1b62531747a33bb2503de02710ed
username <youremail@domain.com>
2022-08-24 18:06:34 -0400

    exercise1

---
+++ exercises.py
@@ -0,0 +1,9 @@
+#Exercise 1
+#Create a list named students containing some student names (strings).
+#Print out the second student's name.
+#Print out the last student's name.
+
+
+students = ['Vieng', 'Yue', 'Jennifer', 'Nicole', 'KeiLee', 'Katharyn']
+print(students[1])
+print(students[-1]) # neg. integer to index from the end of a list
