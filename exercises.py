b42c4f669b6cba2ff13372a0c716dfb360c44f1f
username <youremail@domain.com>
2022-08-24 18:19:31 -0400

    exercise 2

--- exercises.py
+++ exercises.py
@@ -3,7 +3,16 @@
 #Print out the second student's name.
 #Print out the last student's name.
 
-
 students = ['Vieng', 'Yue', 'Jennifer', 'Nicole', 'KeiLee', 'Katharyn']
 print(students[1])
 print(students[-1]) # neg. integer to index from the end of a list
+
+
+# Exercise 2
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Use a for loop to print out the string "food goes here is a good food".
+
+foods = 'Pizza', 'Pasta', 'Hamburgers', 'Salads', 'Ramen', 'Soup'
+#print(type(foods))
+for food in foods:
+    print(f'{food} is a good food')
