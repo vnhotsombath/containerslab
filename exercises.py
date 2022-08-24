851974a5fea9f5e3bc35091cdca62101ff2dff27
username <youremail@domain.com>
2022-08-24 18:54:57 -0400

    exercise 4

--- exercises.py
+++ exercises.py
@@ -23,5 +23,23 @@ for food in foods:
 
 #range() returns the sequence of the given number between the given range
 #range(start,stop) takes two arguments where the series of num. will begin and end
+
+
 for f in range(-2, 0):
     print(foods[f])
+
+
+# Exercise 4
+# Create a dictionary named home_town containing the keys of city, state and population.
+# Print a string with this format:
+# "I was born in city, state - population of population"
+
+
+home_town = {
+    'city': 'Atlanta',
+    'state': 'GA',
+    'population': '6,000,000'
+}
+
+print(f"I was born in {home_town['city']}, {home_town['state']} - popluation of {home_town['population']}") 
+#I kept getting a syntax error for the first home_town bracket, but I changed my single quotations to double and it got rid of the error???
